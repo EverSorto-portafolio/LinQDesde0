babf0b1557f26eb6864d1a72936527c62bdc1bc5
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-07-23 08:51:57 -0600

    Creacion del proyecto y consulta basica comparativa entre linq y obtener el recorrido de un arreglo normal

---
+++ IntroduccionLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿string []palabras;
+palabras = new string[] { "gato", "perro", "lagarto", "tortuga", "cocdrilo","serpiente", "123456789" };
+Console.WriteLine("Mas de 5 letras");
+
+List<string> resultado = new List<string>();
+
+foreach (string str in palabras) {
+    if (str.Length >5) {
+        resultado.Add(str);
+    }
+}
+
+foreach (var r in resultado) 
+    Console.WriteLine(r);
+
+#region utilizando Linq
+
+Console.WriteLine("-----------------------------------------------------");    
+IEnumerable<string> list = from r in palabras where r.Length > 8 select r;
+foreach(var listado in list)
+    Console.WriteLine(listado);
+Console.WriteLine("-----------------------------------------------------");
+
+
+
+
+#endregion
